d6b4b7e79a0311d433def1ee070cb919369c1848
vickyruud <aprajit.k@gmail.com>
2024-03-22 10:02:10 -0400

    change ui

--- app/page.js
+++ app/page.js
@@ -2,10 +2,10 @@ import Image from "next/image";
 import { Button } from "@/stories/Button";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black">
       <div>
-        <Button secondary label="button" size="large" />
-        Hello THere
+        <Button primary label="button" size="large" />
+        Hello
       </div>
     </main>
   );
