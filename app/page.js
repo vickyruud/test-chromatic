ad00f9ea2a036e2ff7547832124acb350a56a0d9
vickyruud <aprajit.k@gmail.com>
2024-03-22 10:13:47 -0400

    change bg color

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { Button } from "@/stories/Button";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black">
+    <main className="flex min-h-screen flex-col items-center justify-between p-52 bg-red-500">
       <div>
         <Button primary label="button" size="large" />
         Hello
