223d037ccce6b2d51a4f28660e964c18b63479bc
vickyruud <aprajit.k@gmail.com>
2024-03-22 09:59:49 -0400

    update button

--- app/page.js
+++ app/page.js
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
       <div>
-        <Button primary label="button" size="large" />
-        Hello
+        <Button secondary label="button" size="large" />
+        Hello THere
       </div>
     </main>
   );
