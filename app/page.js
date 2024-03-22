a5602c6f4a75f4fa4df043ce92cc43c9d3f47bd1
vickyruud <aprajit.k@gmail.com>
2024-03-22 10:20:59 -0400

    button bg red

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-52 bg-green-300">
       <div>
-        <Button primary label="button" size="small" />
+        <Button primary backgroundColor="red" label="button" size="small" />
         <Button primary label="button" size="medium" />
         <Button primary label="button" size="large" />
         <Button secondary label="button" size="large" />
