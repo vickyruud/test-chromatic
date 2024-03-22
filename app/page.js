c8a4aeff3efa5a78a01593b438fb8a8f4c8c5038
vickyruud <aprajit.k@gmail.com>
2024-03-22 10:42:11 -0400

    add paragraph

--- app/page.js
+++ app/page.js
@@ -2,13 +2,14 @@ import Image from "next/image";
 import { Button } from "@/stories/Button";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-52 bg-red-300">
+    <main className="flex min-h-screen flex-col items-center justify-end p-52 bg-red-300">
       <div>
         <Button primary label="button" size="small" />
         <Button primary label="button" size="medium" />
         <Button primary label="button" size="small" />
         <Button secondary label="button" size="medium" />
         Hello
+        <p>This is a new P to test</p>
       </div>
     </main>
   );
