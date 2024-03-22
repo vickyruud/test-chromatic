2b4ef3db2543cccff5482a258ba25bab21ffcdce
vickyruud <aprajit.k@gmail.com>
2024-03-22 10:39:23 -0400

    bg red

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { Button } from "@/stories/Button";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-52 bg-green-300">
+    <main className="flex min-h-screen flex-col items-center justify-between p-52 bg-red-300">
       <div>
         <Button primary label="button" size="small" />
         <Button primary label="button" size="medium" />
